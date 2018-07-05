1d9f50fcdaad651532caa806d106d98d3aa75614
Josh Pospisil <josh.pospisil@gmail.com>
2018-07-05 17:06:18 -0500

    Added example list to accounts component

--- js/components/accounts/Accounts.js
+++ js/components/accounts/Accounts.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux'
-import { Text } from 'react-native';
+import { bindActionCreators } from 'redux';
+import { Text, FlatList } from 'react-native';
+import { ListItem } from 'react-native-elements';
 
 class Accounts extends React.Component {
   render() {
     return (
-        <Text>
-            Accounts
-        </Text>
+            <FlatList
+                data={[
+                    { id: "1", name: "Testing 123", balance: "21.08" },
+                    { id: "2", name: "Testing 456", balance: "52.01" }
+                ]}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item }) => (
+                    <ListItem
+                        title={`${item.name} title ${item.id}`}
+                        subtitle={`${item.name} subtitle`}
+                        hideChevron={true}
+                        rightTitle={`$${item.balance}`}
+                    />
+                )}
+            />
     );
   }
 };
