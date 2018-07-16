68c299dd469aa721bed588776003eeffa9177601
Josh Pospisil <josh.pospisil@gmail.com>
2018-07-16 13:38:18 -0500

    Moved account list to accounts reducer

--- js/components/accounts/Accounts.js
+++ js/components/accounts/Accounts.js
@@ -1,36 +1,34 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { Text, FlatList } from 'react-native';
-import { ListItem, Avatar } from 'react-native-elements';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {Text, FlatList} from 'react-native';
+import {ListItem, Avatar} from 'react-native-elements';
 
 class Accounts extends React.Component {
-  render() {
-    return (
+    render() {
+        const {accounts} = this.props;
+
+        return (
             <FlatList
-                data={[
-                    { id: "1", name: "Testing 123", balance: "21.08" },
-                    { id: "2", name: "Testing 456", balance: "52.01" }
-                ]}
+                data={accounts}
                 keyExtractor={(item, index) => index.toString()}
-                renderItem={({ item }) => (
+                renderItem={({item}) => (
                     <ListItem
-                        title={`${item.name} title ${item.id}`}
+                        title={`${item.name} title`}
                         subtitle={`${item.name} subtitle`}
                         hideChevron={true}
                         rightTitle={`$${item.balance}`}
-                        avatar={<Avatar rounded title={item.name.substr(0,1)} source={null} />}
+                        avatar={<Avatar rounded title={item.name.substr(0, 1)} source={null}/>}
                     />
                 )}
             />
-    );
-  }
+        );
+    }
 };
 
 const mapStateToProps = (state) => {
     return {
-        // active: state.ui.active,
-        // items: state.ui.navigation.bottom
+        accounts: state.accounts.accounts,
     };
 };
 
--- js/reducers/account-reducer.js
+++ js/reducers/account-reducer.js
@@ -1,7 +1,10 @@
 import * as AccountActionTypes from '../actions/account-action-types';
 
 let initialState = {
-
+    accounts: [
+        { id: "1", name: "Testing 123", balance: "21.08" },
+        { id: "2", name: "Testing 456", balance: "52.01" }
+    ]
 };
 
 export default (state = initialState, action) => {
